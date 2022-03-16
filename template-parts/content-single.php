b0b47706a2486de72325980f7771ef171bc92560
felipeandrademorais <flpiandrade@gmail.com>
2022-03-15 21:07:46 -0300

    Visual changes on cards for the inicial page.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,11 +1,32 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header mb-4">
+	<!--Title-->
+	<header class="text-center">
 		<?php the_title( sprintf( '<h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 		<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
 	</header>
 
-	<div class="entry-content">
+	<!--image-->
+	<div class="container w-full max-w-6xl mx-auto bg-white bg-cover mt-8 rounded" style="background-image:url('https://source.unsplash.com/collection/1118905/'); height: 75vh;">
+	</div>
+	
+
+	<div class="bg-white w-full p-8 md:p-24 text-xl md:text-2xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
 		<?php the_content(); ?>
+
+		<!--Subscribe-->	
+		<div class="container font-sans bg-green-100 rounded mt-8 p-4 md:p-24 text-center">
+			<h2 class="font-bold break-normal text-2xl md:text-4xl">Subscribe to Ghostwind CSS</h2>
+			<h3 class="font-bold break-normal font-normal text-gray-600 text-base md:text-xl">Get the latest posts delivered right to your inbox</h3>
+			<div class="w-full text-center pt-4">
+				<form action="#">
+					<div class="max-w-sm mx-auto p-1 pr-0 flex flex-wrap items-center">
+						<input type="email" placeholder="youremail@example.com" class="flex-1 appearance-none rounded shadow p-3 text-gray-600 mr-2 focus:outline-none">
+						<button type="submit" class="flex-1 mt-4 md:mt-0 block md:inline-block appearance-none bg-green-500 text-white text-base font-semibold tracking-wider uppercase py-4 rounded shadow hover:bg-green-400">Subscribe</button>
+					</div>
+				</form>
+			</div>
+		</div>
+		<!-- /Subscribe-->
 
 		<?php
 			wp_link_pages(
@@ -14,7 +35,7 @@
 					'after'       => '</div>',
 					'link_before' => '<span>',
 					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'tailpress' ) . ' </span>%',
+					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'enfermagem' ) . ' </span>%',
 					'separator'   => '<span class="screen-reader-text">, </span>',
 				)
 			);
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,41 +1,32 @@
+<div class="p-4 max-w-sm h-full">
+	<a href="<?php echo esc_url( get_permalink()); ?>" class="flex-1 flex flex-wrap no-underline space-y-4 hover:no-underline">
+		<img src="https://source.unsplash.com/collection/225/800x600" class="h-64 w-full rounded-t pb-6">
+		<p class="w-full text-gray-600 text-xs md:text-sm px-6">
+			<?php 
+				$categories = get_the_category();
 
-<div class="p-4 md:w-1/3">
-	<div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-		<img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog">
-		<div class="p-6">
-			<h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-			<h1 class="title-font text-lg font-medium text-gray-900 mb-3" style="height: 100%">
-				<?php 
-					the_title(
-						sprintf( 
-							'<h2 class="entry-title text-2xl md:text-3xl font-extrabold leading-tight mb-1"><a href="%s" rel="bookmark">', 
-							esc_url( get_permalink() ) ), 
-							'</a></h2>' 
-						); 
-				?>	
-			</h1>
-			<p class="leading-relaxed mb-3"><?php the_excerpt(); ?></p>
-			<div class="flex items-center flex-wrap ">
-				<a href="<?php echo get_the_permalink(); ?>" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
-				<svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-					<path d="M5 12h14"></path>
-					<path d="M12 5l7 7-7 7"></path>
-				</svg>
-				</a>
-				<span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-					<svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-						<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-						<circle cx="12" cy="12" r="3"></circle>
-					</svg>
-					1.2K
-				</span>
-				<span class="text-gray-400 inline-flex items-center leading-none text-sm">
-					<svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-						<path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-					</svg>
-					6
-				</span>
-			</div>
+				foreach($categories as $category) {
+					echo "<span> " . $category->name . "</span>";
+				}
+				?>
+		</p>
+		<div class="w-full font-bold text-xl text-gray-900 px-6">
+			<h2 class="entry-title text-2xl md:text-2xl font-extrabold leading-tight mb-1">
+				<?php the_title(); ?>
+			</h2>
+		</div>
+		<p class="text-gray-800 font-serif text-base px-6 mb-1">
+			<?php  
+				$content = strip_tags(get_the_content());
+				echo substr($content, 0, 300) . " <span> [...] </span> ";
+			?>
+		</p>
+	</a>
+	
+	<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+		<div class="flex items-center justify-between">
+			<img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author">
+			<p class="text-gray-600 text-xs md:text-sm">1 MIN READ</p>
 		</div>
 	</div>
 </div>
